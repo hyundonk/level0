62cc47c3ccb1c12518bd1094ceb80edd807fea2b
Laurent <laurent.lesle@microsoft.com>
2019-10-16 19:14:23 +0800

    Remove deprecated attributes

--- launchpad_opensource/storage.tf
+++ launchpad_opensource/storage.tf
@@ -37,7 +37,7 @@ resource "azurerm_storage_account" "stg" {
 
 resource "azurerm_storage_container" "tfstate" {
   name                  = "tfstate"
-  resource_group_name   = "${azurerm_resource_group.rg.name}"
+  # resource_group_name   = "${azurerm_resource_group.rg.name}"
   storage_account_name  = "${azurerm_storage_account.stg.name}"
   container_access_type = "private"
 }
